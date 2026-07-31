9796019820c56fe88b19b7acae75c9b675f00a28
Ronaldo Bordin Pugina <ronaldobordin2016@gmail.com>
2026-07-31 13:14:56 -0300

    exercicio 2 com funcoes

---
+++ exercicio1.c
@@ -0,0 +1,32 @@
+/*A nota final de um estudante é calculada apartir de 3 notas atribuídas respectivamente de um trabalho de laborátorio, 
+uma avaliação semestral e a um exame final. Com pesos 2, 3 e 5 respectivamente.
+Faça um programa que receba 3 notas, mostre a média ponderada e o seguinte conceito: A(10-8) B(8-7) C(7-6) D(6-5) E(5-0). */
+
+#include<stdio.h>
+
+int main(){
+    float trab_lab, aval_sem, exam_fin, nota_final;
+
+    printf("Digite a nota do trabalho de laboratorio: \n");
+    scanf("%f", &trab_lab);
+    printf("Digite a nota da avaliacao semestral: \n");
+    scanf("%f", &aval_sem);
+    printf("Digite a nota do exame final: \n");
+    scanf("%f", &exam_fin);
+
+    nota_final = ((trab_lab * 2) + (aval_sem * 3) + (exam_fin * 5)) / 10;
+    
+    if(nota_final >= 8){
+        printf("Nota final: A\n");
+    }else if(nota_final >= 7){
+        printf("Nota final: B\n");
+    }else if(nota_final >= 6){
+        printf("Nota final: C\n");
+    }else if(nota_final >= 5){
+        printf("Nota final: D\n");
+    }else{
+        printf("Nota final: E\n");
+    }
+
+    return 0;
+}
---
+++ exercicio2.C
@@ -0,0 +1,41 @@
+/*O custo de um carro novo ao consumidor final é o preço de fábrica somado ao percentual de lucro do distribuidor, 
+acrescido dos impostos aplicados ao preço de fábrica. Faça um programa que receba o preço de fábrica de um veículo,
+o percentual de lucro do distribuidor e o percentual de impostos. Em cada item, crie uma função distinta para calcular e retornar:  
+a) o valor correspondente ao lucro do distribuidor;  
+b) o valor correspondente aos impostos;  
+c) o preço final do veículo.
+Após criar cada uma das funções, desenvolva um algoritmo que declare e invoque cada uma das funções, mostrando o lucro do distribuidor,
+os impostos e o valor final do veículo.*/
+
+#include<stdio.h>
+
+    float profit (float fac_price, float seller_percentage) {
+        return fac_price * seller_percentage;
+    }
+
+    float taxes (float fac_price, float tax_percentage) {
+        return fac_price * tax_percentage;
+    }
+    
+    float final_price (float fac_price, float taxes,float profit) {
+        return fac_price + taxes + profit;
+    }
+
+int main(){
+    float fac_price, sel_percentage, tax_percentage,  p, t, fp;
+
+    printf("Type the factory price of the veichle: \n");
+    scanf("%f", &fac_price);
+    printf("Type the percentage of the seller: \n");
+    scanf("%f", &sel_percentage);
+    printf("Type the tax percentage: \n");
+    scanf("%f", &tax_percentage);
+
+    p = profit(fac_price, sel_percentage);
+    t = taxes(fac_price, tax_percentage);
+
+    fp = final_price(fac_price, t, p);
+
+    printf("The final price of the veichle is: %.2f\n", fp);
+}
+
