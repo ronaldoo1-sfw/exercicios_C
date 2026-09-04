2219ee294e8dc091725e4507f91028177fb994b3
Ronaldo Bordin Pugina <ronaldobordin2016@gmail.com>
2026-09-04 14:18:46 -0300

    Terminando a 1 lista de ponteiros

--- lista_ponteiros.c
+++ lista_ponteiros.c
@@ -78,4 +78,84 @@ int main(){
     
 return 0;
 }
+
+6-Declarar um ponteiro para float e alterar seu valor.
+#include <stdio.h>
+
+int main(){
+    float numero = 3.14;
+    float *ponteiro = &numero;
+    
+    *ponteiro = 6.28;
+    
+    printf("O valor e: %.2f", *ponteiro);
+    
+return 0;
+}
+
+7-Criar um ponteiro nulo e verificar se é NULL.
+#include <stdio.h>
+
+int main(){
+    int *ponteiro = NULL;
+    
+    if(ponteiro == NULL){
+        printf("E nulo!");
+    }
+    
+return 0;
+}
+
+8-Passar um array para uma função usando ponteiro.
+#include <stdio.h>
+
+void imprimir(int *array){
+    int i;
+    
+    for(i=0; i<5; i++){
+        printf("%d\n", *(array + i));
+    }
+    
+}
+
+int main()
+{
+    int array[5] = {1, 2, 3, 4, 5};
+    
+    imprimir(array);
+
+
+    return 0;
+}
+
+9-Incrementar valores de um array usando ponteiro.
+#include <stdio.h>
+
+int main()
+{
+    int array[5] = {1, 2, 3, 4, 5}, i;
+    
+    for(i=0; i<5; i++){
+        *(array + i) = *(array + i) + 1;
+        printf("O valor e: %d\n", *(array + i));
+    }
+
+
+    return 0;
+}
+
+10-Imprimir os endereços de elementos de um array usando ponteiro.
+#include <stdio.h>
+
+int main()
+{
+    int array[5] = {1, 2, 3, 4, 5}, i;
+    
+    for(i=0; i<5; i++){
+        printf("O valor endereco de %d e: %d\n", *(array + i), (array + i));
+    }
+
+
+return 0;
+}
 */
